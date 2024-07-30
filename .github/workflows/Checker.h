b10ab879b4904df51370609a220b1aa687ec4c32
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-07-30 22:51:43 +0530

    Create Checker.h

---
+++ .github/workflows/Checker.h
@@ -0,0 +1,36 @@
+// Checker.h
+#ifndef CHECKER_H
+#define CHECKER_H
+
+#include <string>
+
+
+enum ParameterStatus {
+    LOW_BREACH,
+    LOW_WARNING,
+    NORMAL,
+    HIGH_WARNING,
+    HIGH_BREACH
+};
+
+struct BreachRange {
+    float lowBreach;
+    float highBreach;
+};
+
+struct WarningRange {
+    float lowWarning;
+    float highWarning;
+};
+
+ParameterStatus getBreachStatus(float value, const BreachRange& range);
+ParameterStatus getWarningStatus(float value, const WarningRange& range);
+ParameterStatus getParameterStatus(float value, const BreachRange& breachRange, const WarningRange& warningRange);
+std::string translateStatusToMessage(const std::string& parameter, ParameterStatus status);
+
+bool checkTemperature(float temperature);
+bool checkSoc(float soc);
+bool checkChargeRate(float chargeRate);
+bool batteryIsOk(float temperature, float soc, float chargeRate);
+
+#endif
