47e0c67827f771a989ce69600dbb010e3bcec1f7
nikhilkj1404 <nikhilkj0414@gmail.com>
2024-08-01 11:32:36 +0530

    Update Checker.Tests.cpp

--- Checker.Tests.cpp
+++ Checker.Tests.cpp
@@ -2,12 +2,31 @@
 #include <gtest/gtest.h>
 #include "Checker.h"
 
-// Test overall battery status
-TEST(BatteryTest, TestBatteryIsOk) {
-    EXPECT_TRUE(batteryIsOk(25, 70, 0.5));
-    EXPECT_FALSE(batteryIsOk(50, 70, 0.5));
-    EXPECT_FALSE(batteryIsOk(25, 10, 0.5));
-    EXPECT_FALSE(batteryIsOk(25, 70, 0.9));
-    EXPECT_TRUE(batteryIsOk(2.25, 24, 0.76));
+TEST(BatteryTest, NormalConditions) {
+    EXPECT_TRUE(batteryIsOk(25.0, 50.0, 0.5));
+}
+
+TEST(BatteryTest, LowWarningConditions) {
+    EXPECT_TRUE(batteryIsOk(0 + 45 * 0.05 + 0.1, 50.0, 0.5));
+    EXPECT_TRUE(batteryIsOk(25.0, 20 + 80 * 0.05 + 1, 0.5));
+    EXPECT_TRUE(batteryIsOk(25.0, 50.0, 0.8 - 0.8 * 0.05 - 0.01));
+}
+
+TEST(BatteryTest, HighWarningConditions) {
+    EXPECT_TRUE(batteryIsOk(45 - 45 * 0.05 - 0.1, 50.0, 0.5));
+    EXPECT_TRUE(batteryIsOk(25.0, 80 - 80 * 0.05 - 1, 0.5));
+    EXPECT_TRUE(batteryIsOk(25.0, 50.0, 0.8 - 0.8 * 0.05 - 0.01));
+}
+
+TEST(BatteryTest, LowBreachConditions) {
+    EXPECT_FALSE(batteryIsOk(-1.0, 50.0, 0.5));
+    EXPECT_FALSE(batteryIsOk(25.0, 10.0, 0.5));
+    EXPECT_FALSE(batteryIsOk(25.0, 50.0, -0.1));
+}
+
+TEST(BatteryTest, HighBreachConditions) {
+    EXPECT_FALSE(batteryIsOk(46.0, 50.0, 0.5));
+    EXPECT_FALSE(batteryIsOk(25.0, 90.0, 0.5));
+    EXPECT_FALSE(batteryIsOk(25.0, 50.0, 1.0));
 }
 
